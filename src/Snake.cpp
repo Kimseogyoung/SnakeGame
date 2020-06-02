dce20f89e962fa9a89cb1acde8babb507c65f0cf
dulsik2 <lsy30529@gmail.com>
2020-06-02 16:53:36 +0900

    Update Snake.cpp 1.1
    Snake.cpp 1.1
    * printsnake()(스네이크 출력 함수), addbody()(스네이크 몸 길이 증가 함수) 추가
    * run() 함수 변경: 시간 제어 및 키 입력 판단 방식 변경
    - 시간 제어 관련 함수 변경: clock() -> time()
    - conio.h의 kbhit() 대신 nodelay()로 getch() 딜레이를 없앰
    * test용 main() 추가
    * 주석 추가

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <time.h>
 #include <ncurses.h>
-#include <conio.h>
 using namespace std;
-
-#define UP 72
-#define DOWN 80
-#define LEFT 75
-#define RIGHT 77
 
 struct position{int x, y;};
 class Element{
@@ -17,32 +11,59 @@ private:
   Element* next;
 public:
   Element(int r, int c): x(r), y(c){next = 0;}
+friend class Snake;
 };
 
 class Snake{
 private:
-  Element* head;
-  clock_t mvSpan;
-  int dir;
+  Element* head;   // SLL으로 몸체 구현
+  time_t mvSpan;
+  int dir;   // head의 방향
   int size;
-  position offset[4];
+  position offset[4];   // 이동에 이용할 이동좌표
 public:
   Snake(){
-    head = new Element(10, 10);
-    Element* ptr = head;
-    for (int i = 0; i<2; i++){
-      Element*
-    }
+    head = new Element(10, 10);   // 초기 생성 위치
     dir = 0;
-    size = 3;
-    mvSpan = clock();
+    size = 1;
+    mvSpan = time(NULL);
     offset[0].x = -1;   offset[0].y = 0;   // UP
     offset[1].x = 0;   offset[1].y = 1;   // RIGHT
     offset[2].x = 0;   offset[2].y = -1;   // LEFT
     offset[3].x = 1;   offset[3].y = 0;   // DOWN
+    addbody();
+    addbody();
   }
+
+  // 화면에 스네이크 출력
+  void printsnake(){
+    clear();
+    Element* p=head;
+    while(p){
+      // mvprintw(i,j,"\u2B1C")
+      mvprintw(p->x,p->y,(p==head ? "\u2B1B" : "\u2B1C"));
+      p=p->next;
+    }
+  }
+
+  // 스네이크 몸길이 1증가
+  void addbody(){
+      Element* p=head;
+      while(p->next){
+        p=p->next;
+      }
+      Element* tmp = new Element( p->x - offset[dir].x, p->y - offset[dir].y);
+      p->next=tmp;
+      size++;
+      printsnake();
+  }
+
+  // 스네이크 head의 방향을 얻어오는 함수
+  int getdir(){return dir;}
+
+  // 스네이크를 이동시키는 함수
   void move(int d){
-    Element* p, q;
+    Element* p, *q;
     Element* tmp = new Element(offset[d].x + head->x, offset[d].y + head->y);
     p = head;
     while (p->next){
@@ -50,40 +71,50 @@ public:
       p = p->next;
     }
     q->next = 0;
-    delete p;
+    delete p;   // 맨 끝 삭제
     tmp->next = head;
-    head = tmp;
-    dir = d;
+    head = tmp;   // 새 몸 추가 및 head 변경
+    dir = d;   // head의 방향 설정
   }
+
+// 스네이크 이동 제어 함수
 friend void run(Snake& s);
 };
 
+// nodelay()함수 통해 getch() 딜레이 없앰 + time(NULL) 이용해 시간 제한
 void run(Snake& s){
+  bool terminate = true;
+  int key;
+  int dir = s.dir;
+  nodelay(stdscr, TRUE);
+  while (terminate){
+    while (time(NULL) - s.mvSpan < 0.5){
+      key = getch();
+      if (key == KEY_UP) dir = 0;
+      else if (key == KEY_RIGHT) dir = 1;
+      else if (key == KEY_LEFT) dir = 2;
+      else if (key == KEY_DOWN) dir = 3;
+    }
+    s.move(dir);
+    s.printsnake();
+    s.mvSpan = time(NULL);
+    refresh();
+  }
+}
+
+int main()
+{
+  setlocale(LC_ALL, "");
+  initscr();
   keypad(stdscr, TRUE);
   noecho();
-  while (gameover){
-    while (clock() - s.mvSpan < 500){
-      if (khbit()){   // 키보드 입력 들어오면
-        char key = getch();   // 키보드의 경우 224 75 이런 식으로 두 번 들어옴
-        if (key = 224){   // 방향키이면
-          key = getch();   // 방향키 종류 구별
-          switch(key){
-            case UP:
-              s.move(0);
-              break;
-            case RIGHT:
-              s.move(1);
-              break;
-            case LEFT:
-              s.move(2);
-              break;
-            case DOWN:
-              s.move(3);
-              break;
-          }
-        }
-      }
-    }
-    move(s.dir);
-  }
+  curs_set(0);
+  Snake snake;
+  //snake.addbody();
+  run(snake);
+
+  nodelay(stdscr, FALSE);
+  getch();
+  endwin();
+  return 0;
 }
