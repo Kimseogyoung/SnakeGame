b43c293dfb048fe63991b0674fae0a688ad88285
dulsik2 <52528004+dulsik2@users.noreply.github.com>
2020-06-06 13:36:33 +0900

    Update map
    Gate용

--- src/map.cpp
+++ src/map.cpp
@@ -3,7 +3,7 @@
 #include <locale.h>
 using namespace std;
 
-int map_array[21][40] = {{2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2},
+int map_array[21][40] = {{2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,7,1,1,1,1,1,1,1,1,2},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -18,42 +18,35 @@ int map_array[21][40] = {{2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-                     {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                     {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2}};
 
-void map(){
-  initscr(); //main window start
-  setlocale(LC_ALL, "");
-  resize_term(200, 400);
-  mvprintw(2,15,"Snake Game"); // y,x
-  start_color();
-  init_pair(1, COLOR_WHITE, COLOR_BLACK); // 글씨색, 배경색
-  init_pair(2, COLOR_GREEN, COLOR_BLACK);
 
+
+// map 변경 -> game window만 얘가 다룸. 메인 윈도우는 메인함수에서 따로 정의
+void map(){
   WINDOW *gamewin = newwin(21, 40, 3, 3); //int nlines, int ncols, int begy, int begx
-  box(gamewin,0,0);
   wbkgd(gamewin, COLOR_PAIR(1));
 
+  // 배열의 크기 = 윈도우의 크기
   for (int r = 0; r < 21; r++){
     for (int c = 0; c < 40; c++){
-      if (map_array[r][c] == 2) {mvwaddch(gamewin,r,c,'+');} //y,x
-      else if (map_array[r][c] == 1 && (r == 0 || r == 20)) {mvwaddch(gamewin,r,c,'-');}
-      else if (map_array[r][c] == 1) {mvwaddch(gamewin,r,c,'|');}
-      else {mvwaddch(gamewin,r,c,' ');}
+      if (map_array[r][c] == 2) {mvwaddch(gamewin,r,c,'+');} // immune wall (y,x)
+      else if (map_array[r][c] == 1 && (r == 0 || r == 20)) {mvwaddch(gamewin,r,c,'-');} // wall
+      else if (map_array[r][c] == 1) {mvwaddch(gamewin,r,c,'|');} // wall
+      else if (map_array[r][c] == 3) {mvwaddch(gamewin,r,c,'@');} // snake head
+      else if (map_array[r][c] == 4) {mvwaddch(gamewin,r,c,'#');} // snake body
+      else if (map_array[r][c] == 5) {mvwaddch(gamewin,r,c,'G');} // Grow item
+      else if (map_array[r][c] == 6) {mvwaddch(gamewin,r,c,'P');} // Poison item
+      else if (map_array[r][c] == 7) {mvwaddch(gamewin,r,c,'X');}
+      else {mvwaddch(gamewin,r,c,' ');} // space
     }
   }
-  wrefresh(gamewin);
-
-  refresh();
-  getch();
-  delwin(gamewin);
-  endwin(); // main window end
-}
-
-int main(){
-  map();
+  wrefresh(gamewin); // game window 변경한 부분 적용
+  // refresh();
+  // return gamewin;
 }
